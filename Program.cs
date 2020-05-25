4829969df4dc3f7486929be618bb4ca18dab4a8f
nikitayaptik11 <65231802+nikitayaptik11@users.noreply.github.com>
2020-05-26 01:39:45 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+namespace Zadanie_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList obj = new ArrayList() { 12, 27.3, 3, "e", 7.7, "t", "S", "A", 777, 1, "j", "H", 55, 14.5, 0 };
+            ArrayList objUpper = new ArrayList(100);
+
+            for (int i = 0; i < obj.Count; i++)
+            {
+                Console.Write(obj[i] + " | ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < obj.Count; i++)
+            {
+                if (obj[i] is string)
+                {
+                    obj.RemoveAt(i);
+                    if (obj[i] is string) i--;
+                }
+            }
+
+            for (int i = 0; i < obj.Count; i++)
+            {
+                Console.Write(obj[i] + " | ");
+            }
+        }
+    }
+}
